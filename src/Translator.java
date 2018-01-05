20b156e7f362a86b0393f7ab84d14b6221cab83c
h@di <hadi@hadisafari.ir>
2018-01-05 13:58:51 +0330

    fix bug in arrayLengthCalculate

--- src/Translator.java
+++ src/Translator.java
@@ -219,7 +219,7 @@ public class Translator {
         instructions.add("lw $a1, 4($sp)");
         popStack();
         instructions.add("mult $a0, $a0, $a1");
-        instructions.add("lw $a0, 4($sp)");
+        instructions.add("lw $a1, 4($sp)");
         popStack();
         instructions.add("add $a0, $a0, $a1");
         pushStack("a0");
