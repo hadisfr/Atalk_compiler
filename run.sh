0559da8fffb4a71db07791c4997058241b425a2e
h@di <hadi@hadisafari.ir>
2018-01-09 19:42:09 +0330

    fix run script

--- run.sh
+++ run.sh
@@ -1,20 +1,24 @@
 base_name=$(basename "${PWD}")
 grammar=${base_name/_*/}
-if [[ $# > 1 ]]; then
-    start=$2
-else
-    start="tokens"
-    token_flag="-token"
+
+if [[ $# == 0 ]]; then
+    echo "usage:
+        $0 <${grammar} file>                make 'out.s' MIPS file
+        $0 <${grammar} file> --tokens       tokenize file
+        $0 [<${grammar} file>] --rm         clean src directory
+    "
+    exit 0
 fi
+
 test_input=$1
 
 pushd src >/dev/null
 rm *.class *.tokens ${grammar}Pass1*.java ${grammar}Pass2*.java *.interp *.s &> /dev/null
-if [[ ${test_input} != "--rm" ]]; then
+if [[ $1 != "--rm" && $2 != "--rm" ]]; then
     antlr4 *.g4
     javac *.java
-    if [[ $# > 1 ]]; then
-        grun ${grammar} ${start} ${token_flag} < ../${test_input}
+    if [[ $2 == "--tokens" ]]; then
+        grun ${grammar}Pass1 tokens -tokens < ../${test_input}
     else
         java ${grammar} "../${test_input}"
     fi
